fa5a4b25965ef4a6969081dd4c333117149fff55
GaMa404 <luzettileonardo@gmail.com>
2024-09-05 16:52:15 -0300

    first commit

---
+++ labi9_teste.js
@@ -0,0 +1,20 @@
+/*const x, y = 5
+x + 10
+
+function somar () {
+  r = x + y
+}
+
+console.log(somar())*/
+
+let x = 5; // definindo x como let pois o valor será modificado 
+const y = 5; // y continua sendo const pois o valor não será modificado
+x += 10; // adicionando +10 ao próprio x (x = x + 10)
+
+function somar() 
+{
+    r = x + y;
+    return r; // retornando o resultado da operação para que apareça no console
+}
+
+console.log(somar());
